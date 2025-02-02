d13426f670f72b2f2048d8155363dec1d14de906
Jhony Freitas <jhony00._@hotmail.com>
2025-02-02 10:27:30 -0300

    feat: Create auth route

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,13 +1,10 @@
 import { Router } from "express";
 import { AuthController } from "../controllers/auth";
-import { AuthMiddleware } from "../middlewares/auth";
 
 const authRouter = Router();
-const authMiddleware = new AuthMiddleware();
 const authController = new AuthController();
 
 authRouter.post('/',
-  authMiddleware.validateFields,
   (req, res) => authController.login(req, res)
 );
 
