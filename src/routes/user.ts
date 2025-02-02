a420eda1d3e1abbdad2247635b4d98ecd71cb46b
Jhony Freitas <jhony00._@hotmail.com>
2025-02-02 10:27:50 -0300

    feat: Add create user middleware

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,10 +1,14 @@
+import { UserMiddleware } from '../middlewares/user';
 import { UserController } from './../controllers/user';
 import { Router } from 'express';
 
 const userRouter = Router();
+const userMiddleware = new UserMiddleware();
 const userController = new UserController();
 
 userRouter.post('/',
+  userMiddleware.validateCreateFields,
+  userMiddleware.createUser,
   (req, res) => userController.create(req, res)
 )
 
