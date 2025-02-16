c7bae2429c142819162622666386e4bb824de737
Jhony Freitas <jhony00._@hotmail.com>
2025-02-15 21:07:28 -0300

    feat: Create jwt helpers

---
+++ src/helpers/jwt.ts
@@ -0,0 +1,28 @@
+import jwt from "jsonwebtoken";
+import { AppError, HttpStatus } from "../error/appError";
+
+const SECRET_KEY = process.env.JWT_SECRET || "seu_segredo_super_seguro";
+
+export function generateToken(userId: string): string {
+   if (!SECRET_KEY) {
+    throw new AppError(
+      "JWT_SECRET environment variable not set.",
+      HttpStatus.INTERNAL_SERVER_ERROR
+    );
+  } 
+  return jwt.sign({ userId }, SECRET_KEY, { expiresIn: "1h" });
+}
+
+export function verifyToken(token: string): any {
+  try {
+    if (!SECRET_KEY) {
+      throw new AppError(
+        "JWT_SECRET environment variable not set.",
+        HttpStatus.INTERNAL_SERVER_ERROR
+      );
+    }
+    return jwt.verify(token, SECRET_KEY);
+  } catch (error) {
+    return null;
+  }
+}
