6d0e4cb2be257dd97e554bd3dc65e872c09d7998
Jhony Freitas <jhony00._@hotmail.com>
2025-03-01 02:59:26 -0300

    feat: Finally user routes

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -8,8 +8,39 @@ const userController = new UserController();
 
 userRouter.post('/',
   userMiddleware.validateCreateFields,
-  userMiddleware.createUser,
+  userMiddleware.create,
   (req, res) => userController.create(req, res)
+)
+
+userRouter.get('/:userId',
+  (req, res) => userController.getById(req, res)
+)
+
+userRouter.post('/sendResetToken',
+  userMiddleware.validateEmail,
+  (req, res) => userController.sendResetToken(req, res)
+)
+
+userRouter.post('/sendConfirmToken',
+  userMiddleware.validateEmail,
+  (req, res) => userController.sendConfirmToken(req, res)
+)
+
+userRouter.post('/confirmToken', 
+  userMiddleware.validateEmail,
+  userMiddleware.validateToken,
+  (req, res) => userController.confirmToken(req, res)
+)
+
+userRouter.post('/confirmAccount', 
+  userMiddleware.validateToken,
+  userMiddleware.confirmAccount,
+  (req, res) => userController.confirmAccount(req, res)
+)
+
+userRouter.post('/resetPassword',
+  userMiddleware.resetPassword,
+  (req, res) => userController.resetPassword(req, res)
 )
 
 export { userRouter };
