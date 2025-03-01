0b3f2b97effc34d147d06723abf2256290433e4b
Jhony Freitas <jhony00._@hotmail.com>
2025-03-01 03:00:59 -0300

    feat: Finally user middleware

--- src/middlewares/user.ts
+++ src/middlewares/user.ts
@@ -14,7 +14,11 @@ export class UserMiddleware {
     this.userModel = new UserModel()
     this.companyModel = new CompanyModel()
     this.validateCreateFields = this.validateCreateFields.bind(this);
-    this.createUser = this.createUser.bind(this);
+    this.create = this.create.bind(this);
+    this.validateEmail = this.validateEmail.bind(this);
+    this.confirmAccount = this.confirmAccount.bind(this);
+    this.validateToken = this.validateToken.bind(this);
+    this.resetPassword = this.resetPassword.bind(this);
   }
 
   async validateCreateFields(req: Request, res: Response, next: NextFunction){
@@ -51,7 +55,7 @@ export class UserMiddleware {
     }
   }
 
-  async createUser(req: Request, res: Response, next: NextFunction) {
+  async create(req: Request, res: Response, next: NextFunction) {
     try {
       const { email, cnpj, companyName } = req.body;
 
@@ -68,6 +72,69 @@ export class UserMiddleware {
       const [nameResponse] = await this.companyModel.findAll({ name: companyName });
       if (nameResponse) {
         throw new AppError('Razão social já cadastrada', HttpStatus.BAD_REQUEST)
+      }
+
+      next()
+    } catch (err) {
+      errorHandler(err as Error, res)
+    }
+  }
+
+  async validateEmail(req: Request, res: Response, next: NextFunction){
+    try {
+      const { email } = req.body
+
+      if (!email) throw new AppError('Email é obrigatório', HttpStatus.BAD_REQUEST) 
+
+      const emailVerified = validate.email(email)
+      if (!emailVerified) {
+        throw new AppError('Email inválido', HttpStatus.BAD_REQUEST)
+      }
+
+      next()
+    } catch (err) {
+      errorHandler(err as Error, res)
+    }
+  }
+
+  async confirmAccount(req: Request, res: Response, next: NextFunction){
+    try {
+      const { id } = req.body
+
+      if (!id) throw new AppError('Id do usuário é obrigatório', HttpStatus.BAD_REQUEST) 
+
+      next()
+    } catch (err) {
+      errorHandler(err as Error, res)
+    }
+  }
+
+  async validateToken(req: Request, res: Response, next: NextFunction){
+    try {
+      const { token } = req.body
+      if (!token) {
+        throw new AppError('Token é obrigatório', HttpStatus.BAD_REQUEST) 
+      }
+
+      if (String(token).length !== 15) {
+        throw new AppError('Formato de token inválido', HttpStatus.BAD_REQUEST)
+      }
+
+      next()
+    } catch (err) {
+      errorHandler(err as Error, res)
+    }
+  }
+
+  async resetPassword(req: Request, res: Response, next: NextFunction){
+    try {
+      const { id, password } = req.body
+      if (!id) {
+        throw new AppError('Id do usuário é obrigatório', HttpStatus.BAD_REQUEST) 
+      }
+
+      if (!password) {
+        throw new AppError('Nova senha é obrigatório', HttpStatus.BAD_REQUEST)
       }
 
       next()
