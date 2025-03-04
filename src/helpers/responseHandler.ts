ccef6c602e24ba00b899bc28f7201d0ee94ab256
Jhony Freitas <jhony00._@hotmail.com>
2025-03-04 19:01:46 -0300

    feat: Remove unuse code

--- src/helpers/responseHandler.ts
+++ src/helpers/responseHandler.ts
@@ -1,9 +1,6 @@
 import { Response } from "express";
 
 export class ResponseHandler {
-  constructor(){
-  }
-
   public success(res: Response, statusCode: number, data: any, message: string) {
     res.status(statusCode).json({ 
       status: statusCode,
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,10 +4,6 @@ import { userRouter } from "./user";
 
 const routes = Router();
 
-// Fazer verificação de token nas rotas
-// Pegar o company e passar no res.locals
-// Atualizar models com company
-
 routes.use('/auth', authRouter);
 routes.use('/user', userRouter);
 
